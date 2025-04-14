5f4833d93b4fe4859ee1278a7f9b4b885dc01a11
Anas Zubair <anas.zubair@pakdata.com>
2025-04-14 11:12:48 +0500

    remove client id from code

--- functions/auth/callback.js
+++ functions/auth/callback.js
@@ -1,6 +1,6 @@
 export async function onRequestGet(context) {
-  const GOOGLE_CLIENT_ID = "1029406386881-egt0onk7rvne2a9sgm1f2d0mrbv49240.apps.googleusercontent.com";
-  const GOOGLE_CLIENT_SECRET = "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";
+  const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+  const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
   const url = new URL(context.request.url);
   const origin = url.origin;
--- functions/auth/login.js
+++ functions/auth/login.js
@@ -1,7 +1,7 @@
 export async function onRequestGet(context) {
   // const { GOOGLE_CLIENT_ID, GOOGLE_REDIRECT_URI } = context.env;
-  const  GOOGLE_CLIENT_ID = "1029406386881-egt0onk7rvne2a9sgm1f2d0mrbv49240.apps.googleusercontent.com";
-  const GOOGLE_CLIENT_SECRET = "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";
+  const  GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+  const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
   const url = new URL(context.request.url);
   const GOOGLE_REDIRECT_URI = url.origin+ '/auth/callback';
   const redirectUri = `https://accounts.google.com/o/oauth2/v2/auth` +
