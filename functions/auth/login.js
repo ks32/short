92a592289b568f6588ba44caf0a4766e1000da8c
Anas Zubair <anas.zubair@pakdata.com>
2025-04-16 10:02:43 +0500

    format create.js

--- functions/auth/login.js
+++ functions/auth/login.js
@@ -1,9 +1,9 @@
 export async function onRequestGet(context) {
   // const { GOOGLE_CLIENT_ID, GOOGLE_REDIRECT_URI } = context.env;
-  const  GOOGLE_CLIENT_ID = context.env.client_id;
+  const GOOGLE_CLIENT_ID = context.env.client_id;
   const GOOGLE_CLIENT_SECRET = context.env.client_secret;
   const url = new URL(context.request.url);
-  const GOOGLE_REDIRECT_URI = url.origin+ '/auth/callback';
+  const GOOGLE_REDIRECT_URI = url.origin + '/auth/callback';
   const redirectUri = `https://accounts.google.com/o/oauth2/v2/auth` +
     `?client_id=${GOOGLE_CLIENT_ID}` +
     `&redirect_uri=${GOOGLE_REDIRECT_URI}` +
