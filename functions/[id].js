a96c99abc86773dacd6af1f855ce40f68e966244
Anas Zubair <anas.zubair@pakdata.com>
2025-06-26 10:47:00 +0500

    add easyurdu domain condition

--- functions/[id].js
+++ functions/[id].js
@@ -75,7 +75,7 @@ export async function onRequestGet(context) {
     ).run();
 
     let redirectUrl = Url.url;
-    if(origin === "https://g.quranmajeed.com"){
+    if (origin === "https://g.quranmajeed.com") {
       if (/android/i.test(userAgent)) {
         redirectUrl = `https://play.google.com/store/apps/details?id=com.pakdata.QuranMajeed&hl=en`;
       } else if (/iphone|ipad|ipod/i.test(userAgent)) {
@@ -84,10 +84,10 @@ export async function onRequestGet(context) {
         redirectUrl = "https://quranmajeed.com/";
       }
     }
-    else if(origin === "https://g.kitabi.pk"){
+    else if (origin === "https://g.kitabi.pk") {
       if (/android/i.test(userAgent)) {
         redirectUrl = `https://play.google.com/store/apps/details?id=com.pakdata.kitabi`;
-      } 
+      }
       // else if (/iphone|ipad|ipod/i.test(userAgent)) {
       //   redirectUrl = "https://apps.apple.com/us/app/quran-majeed-%D8%A7%D9%84%D9%82%D8%B1%D8%A7%D9%86-%D8%A7%D9%84%D9%83%D8%B1%D9%8A%D9%85/id365557665";
       // } 
@@ -95,8 +95,19 @@ export async function onRequestGet(context) {
         redirectUrl = "https://kitabi.pk/";
       }
     }
-    else{
-      redirectUrl = "https://quranmajeed.com/";
+    else if (origin === "https://e.pakdata..com") {
+      if (/android/i.test(userAgent)) {
+        redirectUrl = `https://play.google.com/store/apps/details?id=com.pakdata.easyurdu`;
+      }
+      else if (/iphone|ipad|ipod/i.test(userAgent)) {
+        redirectUrl = "https://apps.apple.com/us/app/easy-urdu-keyboard-editor/id866001472";
+      }
+      else {
+        redirectUrl = "https://easyurdu.pk/";
+      }
+    }
+    else {
+      redirectUrl = "https://pakdata.com/";
     }
 
     return Response.redirect(redirectUrl, 302);
