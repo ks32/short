0f3ce51418b3515b0b7cf293174120cb44445860
Anas Zubair <anas.zubair@pakdata.com>
2025-06-27 10:23:03 +0500

    redirect user to pakdata.com when open / url

--- functions/index.js
+++ functions/index.js
@@ -34,9 +34,11 @@ export async function onRequestGet(context) {
       headers: { "Content-Type": "text/html;charset=UTF-8" },
     });
   } else {
-    return new Response(loginHtml, {
-      headers: { "Content-Type": "text/html;charset=UTF-8" },
-    });
+    // return new Response(loginHtml, {
+    //   headers: { "Content-Type": "text/html;charset=UTF-8" },
+    // });
+    const redirectUrl = "https://pakdata.com/";
+    return Response.redirect(redirectUrl, 302);
   }
 }
 
